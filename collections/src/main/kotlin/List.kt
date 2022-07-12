c562aafcbff5a41be23203a653c53067f20e8523
Geisson de Oliveira <geissonlucaso@gmail.com>
2022-07-12 10:15:03 -0300

    Working with listOf, filter, find.

---
+++ collections/src/main/kotlin/List.kt
@@ -0,0 +1,28 @@
+package org.example.collections
+
+fun main() {
+    val geisson = Employee("Geisson", 2500.0)
+    val patrick = Employee("Patrick", 2000.0)
+    val giulia = Employee("Giulia", 2200.0)
+
+    val listEmployee = listOf<Employee>(geisson, patrick, giulia)
+
+    listEmployee.forEach { println(it) }
+    println("---------------------------")
+    println(listEmployee.find { it.name == "Geisson" })
+    println("---------------------------")
+    val salaryLessThan2400 = listEmployee.filter { it.salary in 0.0..2400.0 }
+    salaryLessThan2400.forEach { employee ->
+        println(employee)
+    }
+    println("---------------------------")
+    val employee = listEmployee.filter { it.name == "Giulia" }
+    println(employee)
+}
+
+data class Employee(
+    val name:String,
+    val salary:Double
+) {
+    override fun toString(): String = "$name, $salary".trimIndent()
+}
